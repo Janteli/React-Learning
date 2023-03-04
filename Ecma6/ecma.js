d8056b00288b84d9f536c17af99b682992a94ef7
Surya Kuru <suraz.kuru@gmail.com>
2023-03-04 21:18:53 +0545

    react-learning

---
+++ Ecma6/ecma.js
@@ -0,0 +1,33 @@
+// <!-- Java's International name - es6 was developed in 2015 and later
+// version in each yeay is upgraded
+// -->
+
+// <!-- var was only to define variable but later let and const is used in es-6.
+// var is funtion scoped and kett is block scoped
+// -->
+
+const biodatas = {
+    Name: 'TirthA', 
+    Age :'28',
+    sex:'male',
+};
+
+res = biodatas.Age
+console.log(res);
+
+// destructuring =- no need to repead biodata.Age 
+const  {Age, sex} = biodatas
+
+console.log(Age);
+console.log(sex);
+console.log("My name is" + sex + "My age is" + Age);
+// template string is uset to replace place inverted comma etc `no "` $before var${variable}
+let showvalue = `My name is ${sex}. My name is ${Age};`;
+
+console.log(showvalue);
+// parameters and arguements
+function funAge (age/*parameters later value will be passed called arguements*/) {
+return age;
+};
+funAge(18/*value is passed called arguements, value is passed to parameter age */);
+console.log(funAge);
