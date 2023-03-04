103a85ce86b342ae0206a6b5ae1b8b189f560971
Surya Kuru <suraz.kuru@gmail.com>
2023-03-04 23:13:53 +0545

    added app

--- Ecma6/ecma.js
+++ Ecma6/ecma.js
@@ -31,3 +31,23 @@ return age;
 };
 funAge(18/*value is passed called arguements, value is passed to parameter age */);
 console.log(funAge);
+
+// arrow function
+
+// etension bable, es6, vs icons
+/*
+install node js and npm-> package manager(check npm -v)
+install react from terminal
+-npm install -g create -react-app
+- create-reat-app-version
+- creat-react-app <projectname>
+
+once creat -g create-react-app(globally)
+to check installed create-react-app --version
+actually creating app
+
+
+cmd
+d: -drive change 
+cd folder name
+*/
